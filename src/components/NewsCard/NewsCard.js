1233d902095c3452cee431f11f7f7daacfa2c79a
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-03 15:17:28 -0700

    feat: lay out static test card component

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import './NewsCard.css';
 
-export default function NewsCard() {
+// TODO: DECIDE PNG OR SVG FOR HOVER AND REMOVE OTHER
+// import NewsCardSaveIcon from '../../images/news-card__save-icon.png';
+import NewsCardSaveIcon from '../../images/news-card__save-icon.svg';
+
+
+export default function NewsCard({ card }) {
     return(
         <>
-            <li className='news-card'>
-                <div className='news-card__image-container'>
-                    <button className='news-card__save'></button>
-                    <div className='news-card__photo' 
-                        // style={{ backgroundImage: `url(${props.card.link})` }} 
-                    // style={{ backgroundImage: 'url(' + 'https://images.unsplash.com/photo-1607501629974-d50127ad7066?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1321&q=80' +')'
-                    // }}
-                    >
+            {/* {card && ( */}
+                <li className='news-card'>
+                    <div className='news-card__image-container'>
+                        <div className='news-card__save-container'>
+                            <div className='news-card__save-helper-text'></div>
+                            <button className='news-card__save'>
+                                <img className='news-card__save-icon' src={NewsCardSaveIcon} />
+                            </button>
+                        </div>
+                        <img className='news-card__photo' src='https://images.unsplash.com/photo-1612392062631-94dd858cba88?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' />
                     </div>
-                </div>
-                <div className='news-card__info-container'>
-                    <p className='news-card__date'>Temp date</p>
-                    <h3 className='news-card__title'>Temp header</h3>
-                    <p className='news-card__description'>Temp description</p>
-                    <p className='news-card__source'>Temp source</p>
-                </div>
-            </li>
+                    <div className='news-card__info-container'>
+                        <p className='news-card__date'>0000</p>
+                        <h3 className='news-card__title'>test</h3>
+                        <p className='news-card__description'>test</p>
+                        <p className='news-card__source'>test</p>
+                    </div>
+                </li>
+            {/* )} */}
         </>
     );
 }
--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 import './NewsCardList.css';
+// import EmptyIcon from '../../images/search-empty.png';
+// import LoadingIcon from '../../images/search-loading.png';
 
-export default function NewsCardList() {
+export default function NewsCardList({ cards }) {
     return(
         <>
             <section className='news-card__list'>
-                <h3 className='news-card__list-title'>Search results</h3>
-                <ul className='news-card__grid'>
-                    {/* {props.cards.map((card) => { */}
-                        <NewsCard />
-                    {/* })} */}
-                    <NewsCard />
-                    <NewsCard />
-                </ul>
-                <button className='news-card__list-button'>Show more</button>
+                {/* {isLoading ? ( */}
+                    {/* <div className='news-card__list-loading-state'>
+                        <img className='news-card__list-loading-icon' src={LoadingIcon} />
+                        <p className='news-card__list-loading-text'>Searching for news...</p>
+                    </div> */}
+                {/* ) : ( */}
+                    <>
+                        {/* {!cards ? (
+                            <div className='news-card__list-empty-state'>
+                                <img className='news-card__list-empty-icon' src={EmptyIcon} />
+                                <h3 className='news-card__list-empty-title'>Nothing found</h3>
+                                <p className='news-card__list-empty-text'>Sorry, but nothing matched your search terms.</p>
+                            </div>
+                        ) : ( */}
+                            <div className='news-card__list-container'>
+                                <h3 className='news-card__list-title'>Search results</h3>
+                                <ul className='news-card__grid'>
+                                    {/* {cards && cards.map((card) => {
+                                        <NewsCard key={card.id} card={card} />
+                                     })} */}
+                                    <NewsCard />
+                                </ul>
+                                <button className='news-card__list-button'>Show more</button>
+                            </div>   
+                    </>
+                {/* )} */}
             </section>
         </>
     )

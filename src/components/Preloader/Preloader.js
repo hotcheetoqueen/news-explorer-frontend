4d5490c57fc37a116f1d88cab1b4188e0a39d216
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-03 14:55:27 -0700

    feat: create preloader component

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Preloader'
+
+export default function Preloader() {
+    return(
+        <>
+        <div className='preloader__container'>
+            <p className="preloader"></p>
+        </div>
+        </>
+    )
+}

6568be695b76b53a411a3c1055c270ee0f4819d7
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-06 23:33:18 -0700

    style: make footer mobile responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import iconGithub from '../../images/icons/github.png';
+import iconGithub from '../../images/icons/github.svg';
 import iconLinkedIn from '../../images/icons/linkedin.svg';
 import './Footer.css';
 
@@ -9,8 +9,10 @@ export default function Footer() {
         <footer className='footer'>
             <p className='footer__copyright'>&copy; 2020 Supersite, Powered by News API</p>
             <ul className='footer__navigation'>
-                <li className='footer__navigation-item'>Home</li>
-                <li className='footer__navigation-item'>Practicum by Yandex</li>
+                <div className='footer__navigation_stacked'>
+                    <li className='footer__navigation-item footer__navigation-item_text'><a className='footer__navigation-item-link' href='/'>Home</a></li>
+                    <li className='footer__navigation-item footer__navigation-item_text'><a className='footer__navigation-item-link' href='https://practicum.yandex.com/' target='_blank'>Practicum by Yandex</a></li>
+                </div>
                 <li className='footer__navigation-item'><button className='footer__navigation-item-button' onClick={()=> window.open('https://github.com/hotcheetoqueen', '_blank')}><img src={iconGithub} alt='Github' /></button></li>
                 <li className='footer__navigation-item'><button className='footer__navigation-item-button' onClick={()=> window.open('https://www.linkedin.com/in/jessicaperelman/', '_blank')}><img src={iconLinkedIn} alt='LinkedIn' /></button></li>
             </ul>

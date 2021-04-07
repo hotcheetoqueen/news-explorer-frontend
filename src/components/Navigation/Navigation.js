85ed851ece6857520b4eba9702f964c90ff76a5a
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-07 07:57:54 -0700

    feat: switch to hamburger on mobile

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Hamburger from '../../images/icons/hamburger-menu.svg';
 import LogInOutIcon from '../../images/nav__logout-icon.png';
 import './Navigation.css';
 
 export default function Navigation({ loggedIn, userName, isSavedResults, handleLogOut, handleLogIn, handleLogInClick, handleLogOutClick, modalOpen }) {
     return(
         <>
+            <Link className='navigation__mobile-menu' onClick={handleLogOutClick} to='/'>
+                <img className='navigation__mobile-hamburger' src={Hamburger} alt='menu' />
+            </Link>
             <ul className='navigation__list'>
                 <li className='navigation__list-item'><a className='navigation__list-link' href='/'>Home</a></li>
                 {/* Add/remove ! before logged in to test opposite state */}

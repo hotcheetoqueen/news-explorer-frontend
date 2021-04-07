f4bd41d15d3e5b2526341c6b4c2ee6cd369f137e
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-07 08:30:08 -0700

    feat: refine hero and nav area responsiveness on mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 export default function Header({ loggedIn, userName, handleLogout }) {
     return (
         <header className='header'>
-            <h1><a className='header__logo' href='/'>NewsExplorer</a></h1>
+            <h1 className='header__logo'><a className='header__logo-link' href='/'>NewsExplorer</a></h1>
             <Navigation loggedIn={loggedIn} userName={userName} handleLogout={handleLogout}/>
         </header>
     )

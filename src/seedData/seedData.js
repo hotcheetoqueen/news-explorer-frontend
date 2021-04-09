e44f0fef48ac8801459a395677e598635b7bffbb
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-08 19:51:24 -0700

    feat: make news cards more responsive and truncate text

--- src/seedData/seedData.js
+++ src/seedData/seedData.js
@@ -2,7 +2,7 @@ export const seedData = [
     {
         date: 'November 4, 2020',
         title: 'Everyone Needs a Special \'Sit Spot\' in Nature',
-        text: 'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
+        description: 'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
         source: 'Treehugger',
         link: 'https://www.treehugger.com/special-sit-spot-nature-5085811',
         image: 'https://images.unsplash.com/photo-1608835175091-947efa60fd88?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
@@ -13,7 +13,7 @@ export const seedData = [
     {
         date: 'February 19, 2019',
         title: 'Nature makes you better',
-        text: 'We all know how good nature can make us feel. We have known it for millennia: the sound of the ocean, the scents of a forest, the way dappled sunlight dances through leaves.',
+        description: 'We all know how good nature can make us feel. We have known it for millennia: the sound of the ocean, the scents of a forest, the way dappled sunlight dances through leaves.',
         source: 'National Geographic',
         link: 'https://www.nationalgeographic.com/travel/features/partner-content-nature-makes-you-better/#:~:text=Nature%20makes%20you%20better&text=We%20are%20beginning%20to%20learn,can%20calm%20and%20connect%20us.',
         image: 'https://images.unsplash.com/photo-1609004866740-d5647459b330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80',
@@ -24,8 +24,8 @@ export const seedData = [
     {
         date: 'October 19, 2019',
         title: 'Grand Teton Renews Historic Crest Trail',
-        text: 'The linking together of the Cascade and Death Canyon trails, at their heads, took place on October 1, 1933, and marked the first step in the realization of a plan whereby the hiker will be...',
-        source: 'National Parks Traveler',
+        description: 'The linking together of the Cascade and Death Canyon trails, at their heads, took place on October 1, 1933, and marked the first step in the realization of a plan whereby the hiker will be...',
+        source: 'National Parks Traveler National Parks Traveler',
         link: 'https://www.nationalparkstraveler.org/2020/10/grand-teton-renews-historic-crest-trail#:~:text=Grand%20Teton%20National%20Park\'s%20first,the%20grit%20to%20get%20there.',
         image: 'https://images.unsplash.com/photo-1608914412956-956942bbab4d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         owner: 'hotcheetoqueen',

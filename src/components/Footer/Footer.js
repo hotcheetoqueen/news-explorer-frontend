2f1abca27b43d274a5dc23f2f615771f05ca0732
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-09 19:42:54 -0700

    fix: make div inside ul an li

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,10 +9,10 @@ export default function Footer() {
         <footer className='footer'>
             <p className='footer__copyright'>&copy; 2020 Supersite, Powered by News API</p>
             <ul className='footer__navigation'>
-                <div className='footer__navigation_stacked'>
+                <li className='footer__navigation_stacked'>
                     <li className='footer__navigation-item footer__navigation-item_text'><a className='footer__navigation-item-link' href='/'>Home</a></li>
                     <li className='footer__navigation-item footer__navigation-item_text'><a className='footer__navigation-item-link' href='https://practicum.yandex.com/' target='_blank' rel='noreferrer'>Practicum by Yandex</a></li>
-                </div>
+                </li>
                 <li className='footer__navigation-item'><button className='footer__navigation-item-button' onClick={()=> window.open('https://github.com/hotcheetoqueen', '_blank')}><img src={iconGithub} alt='Github' /></button></li>
                 <li className='footer__navigation-item'><button className='footer__navigation-item-button' onClick={()=> window.open('https://www.linkedin.com/in/jessicaperelman/', '_blank')}><img src={iconLinkedIn} alt='LinkedIn' /></button></li>
             </ul>

719f50c44b4ef0d1bd94ce4511ea02f0b2cad75a
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-11 21:44:12 -0700

    style: refine dark theme for logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 export default function Header({ loggedIn, openModal, userName, handleLogOut, isSavedResults, theme }) {
     return (
         <header className={`header ${theme ? `header_theme_${theme}` : ''}`}>
-            <h1 className={`header__logo ${theme ? `header_theme_${theme}` : ''}`}><a className={`header__logo-link ${theme ? `header_theme_${theme}` : ''}`} href='/'>NewsExplorer</a></h1>
+            <h1 className='header__logo'><a className={`header__logo-link ${theme ? `header_theme_${theme}` : ''}`} href='/'>NewsExplorer</a></h1>
             <Navigation loggedIn={loggedIn} userName={userName} handleLogOut={handleLogOut} openModal={openModal}  theme={theme} />
         </header>
     )

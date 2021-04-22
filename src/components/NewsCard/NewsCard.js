73737d65322e067b305399d33cee4fac11ebe748
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-21 20:13:10 -0700

    feat: add link to article card to open in new tab

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -35,7 +35,7 @@ export default function NewsCard({ card, loggedIn, isSavedResults, handleSaveCli
                         </div>
                         <img className='news-card__photo' src='https://images.unsplash.com/photo-1612392062631-94dd858cba88?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' alt='News article subject' />
                     </div>
-                    <div className='news-card__info-container'>
+                    <div className='news-card__info-container' href={card.link} rel='noreferrer' target='_blank'>
                         {/* <p className='news-card__date'>{formatDate(card.publishedAt)}</p> */}
                         <p className='news-card__date'>{card.publishedAt}</p>
                         <h3 className='news-card__title'>{card.title}</h3>

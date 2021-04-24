bc3a7053e73c12e07c5e1083ac56d6da77aff32f
hotcheetoqueen <jessicalperelman@gmail.com>
2021-04-23 19:13:30 -0700

    feat: make entire card clickable, including image, not just container

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -9,41 +9,41 @@ export default function NewsCard({ card, loggedIn, isSavedResults, handleSaveCli
         <>
             {card && (
                 <li className='news-card'>
-                    {isSavedResults && (
-                        // <p className="card__keyword">
-                        //     {card.keyword.slice(1)}
-                        // </p>
-                        <div className='news-card__keyword'>Keyword</div>
-                    )}
-                    <div className='news-card__image-container'>
-                        <div className='news-card__save-container'>
-                            {!isSavedResults ?
-                                <button className='news-card__save' onClick={() => handleSaveClick(card)}>
-                                    <div className={`news-card__save-icon ${card.isSaved ? ' news-card__save-icon_saved' : ''}`} src={NewsCardSaveIcon} alt='Save article' />
-                                </button> :
-                                <button className='news-card__save' onClick={() => handleDeleteClick(card)}>
-                                    <img className={`${card.isSaved ? ' news-card__save-icon_delete' : ''}`} src={NewsCardDeleteIcon} alt='Delete article' />
-                                </button>
-                            }
-                            {isSavedResults && (
-                                <div className='news-card__save-helper-text'>Remove from saved</div>
-                            )}
-                        {/* Add/remove ! before logged in to test opposite state */}
-                            {!isSavedResults && (
-                                <div className={`${loggedIn ? 'news-card__save-helper-text' : 'news-card__save-helper-text_hidden'}`} >Sign in to save articles</div>
-                            )}
+                    <a className='news-card__link' href={card.url} rel='noreferrer' target='_blank'>
+                        {isSavedResults && (
+                            // <p className="card__keyword">
+                            //     {card.keyword.slice(1)}
+                            // </p>
+                            <div className='news-card__keyword'>Keyword</div>
+                        )}
+                        <div className='news-card__image-container'>
+                            <div className='news-card__save-container'>
+                                {!isSavedResults ?
+                                    <button className='news-card__save' onClick={() => handleSaveClick(card)}>
+                                        <div className={`news-card__save-icon ${card.isSaved ? ' news-card__save-icon_saved' : ''}`} src={NewsCardSaveIcon} alt='Save article' />
+                                    </button> :
+                                    <button className='news-card__save' onClick={() => handleDeleteClick(card)}>
+                                        <img className={`${card.isSaved ? ' news-card__save-icon_delete' : ''}`} src={NewsCardDeleteIcon} alt='Delete article' />
+                                    </button>
+                                }
+                                {isSavedResults && (
+                                    <div className='news-card__save-helper-text'>Remove from saved</div>
+                                )}
+                            {/* Add/remove ! before logged in to test opposite state */}
+                                {!isSavedResults && (
+                                    <div className={`${loggedIn ? 'news-card__save-helper-text' : 'news-card__save-helper-text_hidden'}`} >Sign in to save articles</div>
+                                )}
+                            </div>
+                            <img className='news-card__photo' src={card.urlToImage} alt='News article subject' />
                         </div>
-                        <img className='news-card__photo' src={card.urlToImage} alt='News article subject' />
-                    </div>
-                    <div className='news-card__info-container'>
-                        <a className='news-card__info-container-link' href={card.url} rel='noreferrer' target='_blank'>
-                            {/* <p className='news-card__date'>{formatDate(card.publishedAt)}</p> */}
-                            <p className='news-card__date'>{card.publishedAt}</p>
-                            <h3 className='news-card__title'>{card.title}</h3>
-                            <p className='news-card__description'>{card.description}</p>
-                            <p className='news-card__source'>{card.source}</p>
-                        </a>
-                    </div>
+                        <div className='news-card__info-container'>
+                                {/* <p className='news-card__date'>{formatDate(card.publishedAt)}</p> */}
+                                <p className='news-card__date'>{card.publishedAt}</p>
+                                <h3 className='news-card__title'>{card.title}</h3>
+                                <p className='news-card__description'>{card.description}</p>
+                                <p className='news-card__source'>{card.source}</p>
+                        </div>
+                    </a>
                 </li>
             )}
         </>

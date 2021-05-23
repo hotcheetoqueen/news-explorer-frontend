bb3c56eadbb9f3a655c271f98884d2c18339377d
hotcheetoqueen <jessicalperelman@gmail.com>
2021-05-22 19:55:51 -0700

    fix: pass handleLogOut to SavedNews for smooth drill down to logout on saved-news

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -5,7 +5,7 @@ import NewsCardList from '../NewsCardList/NewsCardList';
 import SavedNewsHeader from '../SavedNewsHeader/SavedNewsHeader';
 import './SavedNews.css';
 
-export default function SavedNews({ cards, loggedIn, handleLogInClick, handleSaveClick, handleHamburgerClick, isLoading, isSaved }) {
+export default function SavedNews({ cards, loggedIn, handleLogInClick, handleSaveClick, handleHamburgerClick, isLoading, isSaved, handleLogOut }) {
     const history = useHistory();
 
     useEffect(() => {
@@ -17,7 +17,7 @@ export default function SavedNews({ cards, loggedIn, handleLogInClick, handleSav
   
     return (
         <>
-            <SavedNewsHeader cards={cards} handleHamburgerClick={handleHamburgerClick} loggedIn={loggedIn} handleLogInClick={handleLogInClick} />
+            <SavedNewsHeader cards={cards} handleHamburgerClick={handleHamburgerClick} loggedIn={loggedIn} handleLogInClick={handleLogInClick} handleLogOut={handleLogOut} />
             <NewsCardList cards={cards} isSavedResults={true} handleSaveClick={handleSaveClick} loggedIn={loggedIn} isLoading={isLoading} isSaved={isSaved} />
             <Footer />
         </>
